7bbebc52002cc187ff4c49c62a030fdbc082f7c4
Aidan <farinaaidan2@gmail.com>
2023-04-23 20:49:16 -0400

    did local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
+var saveButton = document.querySelector(".btn");
+var textBox = $("textarea")
+var saveKey = "mySaveData"
+var savedData = localStorage.getItem(saveKey);
+
+if(savedData) {
+  textBox.val(JSON.parse(savedData));
+}
+
+$(saveButton).click(function () {
+  var savedData = JSON.stringify(textBox.val());
+  localStorage.setItem(saveKey, savedData);
+});
+ });
+
+
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +38,4 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+ 
